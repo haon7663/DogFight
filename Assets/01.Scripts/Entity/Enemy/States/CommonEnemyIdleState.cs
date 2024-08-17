f9addcc9d1988a3a17d1e8c9eb9479d4ae766181
haon7663 <89632611+haon7663@users.noreply.github.com>
2024-08-17 21:37:13 +0900

    Add Throwing Weapon

--- Assets/01.Scripts/Entity/Enemy/States/CommonEnemyIdleState.cs
+++ Assets/01.Scripts/Entity/Enemy/States/CommonEnemyIdleState.cs
@@ -7,4 +7,27 @@ public class CommonEnemyIdleState : State<CommonEnemy>
     public CommonEnemyIdleState(CommonEnemy owner, StateMachine<CommonEnemy> stateMachine, string animBoolName) : base(owner, stateMachine, animBoolName)
     {
     }
+    
+    public override void Enter()
+    {
+        base.Enter();
+        _owner.MovementCompo.StopImmediately();
+        _owner.AgentCompo.OnMovementEvent += HandleOnMovementEvent;
+        HandleOnMovementEvent(_owner.AgentCompo.MovementDir);
+    }
+
+    private void HandleOnMovementEvent(Vector2 movement)
+    {
+        base.UpdateState();
+        if(movement.magnitude > 0.05f)
+        {
+            _stateMachine.ChangeState(PlayerStateEnum.Move);
+        }
+    }
+
+    public override void Exit()
+    {
+        _owner.AgentCompo.OnMovementEvent -= HandleOnMovementEvent;
+        base.Exit();
+    }
 }
--- Assets/01.Scripts/Entity/Weapon/ThrowingWeapon.cs
+++ Assets/01.Scripts/Entity/Weapon/ThrowingWeapon.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ThrowingWeapon : MonoBehaviour
 {
-
-
+    [SerializeField] private Rigidbody2D rigid;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    public void Initialize(WeaponSO weaponSO)
+    {
+        spriteRenderer.sprite = weaponSO.throwSprite;
+    }
+    
     public void Fire(Vector2 dir)
     {
-
+        rigid.velocity = dir;
     }
 }
