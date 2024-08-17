ea9d4eb48ddc8a67a652c5016d35e4d3bfd65e1b
haon7663 <89632611+haon7663@users.noreply.github.com>
2024-08-18 01:26:39 +0900

    Update EnemyHitState

--- Assets/01.Scripts/Entity/Enemy/States/CommonEnemyHitState.cs
+++ Assets/01.Scripts/Entity/Enemy/States/CommonEnemyHitState.cs
@@ -7,4 +7,12 @@ public class CommonEnemyHitState : State<CommonEnemy>
     public CommonEnemyHitState(CommonEnemy owner, StateMachine<CommonEnemy> stateMachine, string animBoolName) : base(owner, stateMachine, animBoolName)
     {
     }
+    
+    public override void UpdateState()
+    {
+        if (IsTriggerCalled(AnimationTriggerEnum.EndTrigger))
+        {
+            _stateMachine.ChangeState(CommonEnemyStateEnum.Move);
+        }
+    }
 }
